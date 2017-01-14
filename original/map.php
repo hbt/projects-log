10e77b9d634af856d384cab3f7f40fe59ccdf7b4
hbt <hassenbentanfous@gmail.com>
2017-01-14 16:22:55 -0500

    wip

--- original/map.php
+++ original/map.php
@@ -8,6 +8,7 @@ $map = [
             'My history with this bug' => '2',
             'Forking and fixing a bug in 2016' => '3',
             'thinking about vision and end goal' => '4',
+            
 //            'Sharing the fix' => '',
 //            "Feedback and what's next" => '',
 //            "Maintenance is the time sink of all software" => '',
