f0ae62068cf643855163d774775531662078d1a6
hbt <hassenbentanfous@gmail.com>
2017-01-14 14:19:58 -0500

    wip

--- todos.php
+++ todos.php
@@ -2,7 +2,12 @@
 
 $m = [
     'publish on github-pages in a readable format',
-    'write about chromium, mouseless,dotfiles,github-backup -- move stuff from old log here',
     'figure out the tone,philo behind the writing. self censorship? professional? personal? who gives a fuck cauz who will read it?',
+    'write about chromium, mouseless,dotfiles,github-backup -- move stuff from old log here',
+    'deprecate plmap',
+    
+    'add to projects + link',
+    'fix links -- have hbt.github.io redirect to hbtlabs.github.io for projects list --- flag as enhance to migrate away from hbtlabs to hbt',
+    'test comment -- http://stackoverflow.com/questions/4823468/comments-in-markdown',
 ]
 ?>
