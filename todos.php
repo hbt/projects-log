583a00d5b28b2d92eaf0686cfda2fb834b5afddc
hbt <hassenbentanfous@gmail.com>
2017-01-14 13:47:56 -0500

    wip

---
+++ todos.php
@@ -0,0 +1,8 @@
+<?php
+
+$m = [
+    'publish on github-pages in a readable format',
+    'write about chromium, mouseless,dotfiles,github-backup -- move stuff from old log here',
+    'figure out the tone,philo behind the writing. self censorship? professional? personal? who gives a fuck cauz who will read it?',
+]
+?>
