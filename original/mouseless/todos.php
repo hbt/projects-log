f6976a011c8f9eed7ecf41ba58bdc1d9eedbc648
hbt <hassenbentanfous@gmail.com>
2017-01-14 19:08:29 -0500

    wip

---
+++ original/mouseless/todos.php
@@ -0,0 +1,35 @@
+<?php
+
+$todos = [
+    'create master / dev -- master = stable, dev = active dev',
+    'polish existing features implementation',
+    'get contributors to spread maintenance load + generate serependity' => [
+        'read on community building #55' => [
+            
+        ],
+        'improve documentation' => [
+            'fix readme',
+            'add help page listing commands and descriptions',
+            'add wiki for examples of css cvim, configs, custom functions, website configurations,',
+            'get github-pages' => [
+                'what is it',
+                'targeted audience at this stage',
+                'alternatives',
+                'commands',
+                'how to contribute' => [
+                    'answer user questions',
+                    'pull requests',
+                    'spread the word to the right audience',
+                ],
+                'wiki',
+                'faq',
+            ],
+            'create releases - zip files / tag for easy download',
+        ],
+
+        'contact watchers / forkers / pull requesters' => [
+            
+        ],
+    ],
+];
+?>
