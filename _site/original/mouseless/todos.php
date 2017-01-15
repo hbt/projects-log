00c20b2b12416d1b9025d6129bb516a4bec892b6
hbt <hassenbentanfous@gmail.com>
2017-01-15 11:33:55 -0500

    wip

---
+++ _site/original/mouseless/todos.php
@@ -0,0 +1,40 @@
+<?php
+
+$todos = [
+    'create master / dev -- master = stable, dev = active dev',
+    'polish existing features implementation',
+    'get contributors to spread maintenance load + generate serependity' => [
+        'read on community building #55' => [
+            
+        ],
+        'improve documentation' => [
+            'use https://readthedocs.org/ or something similar',
+            'fix readme',
+            'add help page listing commands and descriptions',
+            'add wiki for examples of css cvim, configs, custom functions, website configurations,',
+            'get github-pages' => [
+                'what is it',
+                'targeted audience at this stage',
+                'alternatives',
+                'commands',
+                'how to contribute' => [
+                    'answer user questions',
+                    'pull requests',
+                    'spread the word to the right audience',
+                ],
+                'wiki',
+                'faq',
+            ],
+            'create releases - zip files / tag for easy download',
+        ],
+
+        'contact watchers / forkers / pull requesters' => [
+            
+        ],
+    ],
+    'look into viable business models for open source' => [
+        't-shirts / coffee mugs and other merchandise of mouse hate - https://salt.bountysource.com/checkout/amount?team=neovim',
+        'be listed as a sponsor ',
+    ],
+];
+?>
