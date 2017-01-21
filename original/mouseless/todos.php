9f751afa214f57fb12ae794d005deb3b7aa4dfef
hbt <hassenbentanfous@gmail.com>
2017-01-21 00:12:46 -0500

    auto-update from boot script

--- original/mouseless/todos.php
+++ original/mouseless/todos.php
@@ -1,44 +1,43 @@
 <?php
 
 $todos = [
-    'new branches' => [
-        'create master / dev -- master = stable, dev = active dev',
-        'create google-store branch .. without the chrome// stuff',
-        'document branches',
+    'continue work on doc + polishing features' => [
+        'view NEXT items',
     ],
-    'polish existing features implementation',
-    'get contributors to spread maintenance load + generate serependity' => [
-        'read on community building #55' => [
 
+    'review' => [
+
+        'new branches' => [
+            'create master / dev -- master = stable, dev = active dev',
+            'create google-store branch .. without the chrome// stuff',
+            'document branches',
         ],
-        'improve documentation' => [
-            'use https://readthedocs.org/ or something similar',
-            'fix readme',
-            'add help page listing commands and descriptions',
-            'add wiki for examples of css cvim, configs, custom functions, website configurations,',
-            'get github-pages' => [
-                'what is it',
-                'targeted audience at this stage',
-                'alternatives',
-                'commands',
-                'how to contribute' => [
-                    'answer user questions',
-                    'pull requests',
-                    'spread the word to the right audience',
+        'polish existing features implementation',
+        'get contributors to spread maintenance load + generate serependity' => [
+            'improve documentation' => [
+                'fix readme',
+                'add help page listing commands and descriptions',
+                'add wiki for examples of css cvim, configs, custom functions, website configurations,',
+                'get github-pages' => [
+                    'what is it',
+                    'targeted audience at this stage',
+                    'alternatives',
+                    'commands',
+                    'how to contribute' => [
+                        'answer user questions',
+                        'pull requests',
+                        'spread the word to the right audience',
+                    ],
+                    'wiki',
+                    'faq',
                 ],
-                'wiki',
-                'faq',
+                'create releases - zip files / tag for easy download',
             ],
-            'create releases - zip files / tag for easy download',
-        ],
 
-        'contact watchers / forkers / pull requesters' => [
+            'contact watchers / forkers / pull requesters' => [
 
+            ],
         ],
-    ],
-    'look into viable business models for open source' => [
-        't-shirts / coffee mugs and other merchandise of mouse hate - https://salt.bountysource.com/checkout/amount?team=neovim',
-        'be listed as a sponsor ',
     ],
 ];
 ?>
