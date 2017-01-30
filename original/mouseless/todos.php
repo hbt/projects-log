dc285e2614374e4206153d95f521de78747924f0
hbt <hassenbentanfous@gmail.com>
2017-01-30 11:52:43 -0500

    wip

--- original/mouseless/todos.php
+++ original/mouseless/todos.php
@@ -1,23 +1,33 @@
 <?php
 
 $todos = [
-    'complete quick fixes',
-    'continue work on doc + polishing features' => [
-        'view NEXT items',
-        'change language mouseless regarding contributions - encourage sharing custom functions / frontend functions. put frontend stuff in the tips page and if it needs backend or is too complex in frontend, send pull request ',
-        'map gv gifv',
+    'next'   => [
+        'fixes'                                     => [
+            59,
+            60,
+            57,
+        ],
+
+        'continue work on doc + polishing features' => [
+            'view NEXT items',
+            'change language mouseless regarding contributions - encourage sharing custom functions / frontend functions. put frontend stuff in the tips page and if it needs backend or is too complex in frontend, send pull request ',
+            'map gv gifv',
+            'publish + link projects-log as blog + todos',
+        ],
+
     ],
+
 
     'review' => [
 
-        'new branches' => [
+        'new branches'                                                       => [
             'create master / dev -- master = stable, dev = active dev',
             'create google-store branch .. without the chrome// stuff',
             'document branches',
         ],
         'polish existing features implementation',
         'get contributors to spread maintenance load + generate serependity' => [
-            'improve documentation' => [
+            'improve documentation'                        => [
                 'fix readme',
                 'add help page listing commands and descriptions',
                 'add wiki for examples of css cvim, configs, custom functions, website configurations,',
