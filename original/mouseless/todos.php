8f177052b5756aa46304e940df3257c7f28e268f
hbt <hassenbentanfous@gmail.com>
2017-02-01 14:05:58 -0500

    wip

--- original/mouseless/todos.php
+++ original/mouseless/todos.php
@@ -3,17 +3,29 @@
 $todos = [
     'top'                                           => [
         'rethink metrics and publish on official - nb commands, nb errors, nb contributions/forks (start thanks/credit page)',
+        'metrics' => [
+            // TODO(hbt) NEXT justify metrics
+            'nb contributors or nb contributions H',
+            'nb support requests L',
+            'average response time',
+            'average merge time',
+            'average release time',
+            'frequently used commands justifying their maintenance',
+        ],
 
         'next' => [
             'continue work on doc + polishing features' => [
                 'improve install -- use <<>> and have zip version first',
                 'fix tips/faq page',
+                'add thanks page',
                 'publish + link projects-log as blog + todos',
                 'add github hook to auto-update mouseless.hbtlabs',
                 'map gv gifv',
             ],
 
             'merge https://github.com/dhda/chromium-vim/commit/07e968a65fecf5687850f6a8191610108b3b8538',
+            'add the google version branch',
+            'fix review below',
 
         ],
 
