db08962c8a50dc72ed240a13c7f7da8857e87d79
hbt <hassenbentanfous@gmail.com>
2017-02-03 01:27:41 -0500

    wip

--- original/software/todos.php
+++ original/software/todos.php
@@ -1,13 +1,13 @@
 <?php
 
 $todos = [
-    'top' => [
+    'top'   => [
         'resources for deliberate practice',
         'figure out vision, end result and metrics',
     ],
 
     'reorg' => [
-        'softwarestash'                                                                         => [
+        'softwarestash'                                        => [
             'development cycle and existing tools -- like startupstash - braintstorm ways of eliminating/enhancing the step - clear up the goal behind the step. why do it ',
             'finding similar software ' => [
                 'biggest problem is finding the right keywords from the domain',
@@ -15,37 +15,38 @@ $todos = [
             ],
         ],
 
-        'deliberate practice resources for software engineers'                                  => [
+        'deliberate practice resources for software engineers' => [
             'list of strategies to deliberate practice',
             'focus on exact steps for deliberate practice and clarify what each resource attempts to do',
             'order by effectiveness based on your goals',
         ],
 
-        'write on software'                                                                     => [
+        'write on software'                                    => [
             'remove the word feature from your vocabulary. it is an experiment first, if it doesnt work out. remove it. if it does, then you consider how it would be permanently implemented',
             'organize it like a system instead of goals',
         ],
 
-    ],
-    'get writing outline',
+        'get writing outline',
 
-    'delib practice' => [
+        'delib practice'                                       => [
 
-        'software deliberate practice' => [
-            'trash/accelerate-software.php:2',
-            'trash/code-readability.php:2',
-            'trash/humanscoding.com/humanscoding.com-todos.php:6',
+            'software deliberate practice' => [
+                'trash/accelerate-software.php:2',
+                'trash/code-readability.php:2',
+                'trash/humanscoding.com/humanscoding.com-todos.php:6',
+            ],
+
+            'more research'                => [
+                'https://twitter.com/search?q=software%20deliberate%20practice&src=typd',
+            ],
         ],
-        
-        'more research' => [
-            'https://twitter.com/search?q=software%20deliberate%20practice&src=typd',
+
+
+        'deliberate practice resources for software engineers' => [
+            'find catch title for site -- softwaredeliberatepractice softwareselfimprovement deliberateengineering ',
         ],
     ],
 
-
-    'deliberate practice resources for software engineers' => [
-        'find catch title for site -- softwaredeliberatepractice softwareselfimprovement deliberateengineering ',
-    ],
 ];
 
 ?>
--- todos.php
+++ todos.php
@@ -3,6 +3,9 @@
 $m = [
     'top' => [
         'fix projects-log on hbtlabs  + add hook and disable gh version',
+        'add homepage link',
+        'add analytics',
+        'add to projects list',
     ],
 
     'write on' => [
@@ -52,39 +55,6 @@ $m = [
 
     'admin'    => [
 
-
-        'reorg' => [
-
-
-            'enhance dev'           => [
-                'move scripts to project root and use cd',
-            ],
-
-            'hosting'               => [
-                'review if github pages cache could be invalidated by html headers ',
-                'host on hbtlabs.com fucking tired of github pages and whatever bullshit stops working',
-            ],
-
-            'review doctype'        => [
-                'doctype book vs article == the needed subsection -- maybe change titles?',
-            ],
-
-
-            'add to projects + link',
-
-
-            'enhance github pages ' => [
-                'fix links -- have hbt.github.io redirect to hbtlabs.github.io for projects list --- flag as enhance to migrate away from hbtlabs to hbt',
-                'review todos / enhancements',
-                'add header. link to homepage banner. add analytics if quick',
-                'add linker/ url fixer in files e.g %%5%%',
-                'fix layout ',
-                'add analytics',
-                'include title on top of post page -- from map ',
-                'make SEO friendly',
-            ],
-
-        ]
     ],
 
 
