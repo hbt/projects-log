1811f81e502ba4a16b81770268dbc548b9e11f35
hbt <hassenbentanfous@gmail.com>
2017-02-03 01:40:07 -0500

    unassumed

--- original/map.php
+++ original/map.php
@@ -1,32 +1,74 @@
 <?php
 
 $map = [
-    'dotfiles' => [
-        'previous projects' => '12',
-        'vcsh setup' => '13',
-        'future of dotfiles (brainstorming)' => '14',
-    ],
-    'mouseless' => [
-        'me and the keyboard. a love story.' => '8',
-        'increasing contributors' => [
-            'bringing mouseless to the average user (brainstorming)' => '9',
-            'Stick to advanced users.' => '10',
+
+    'thinking about software'  => [
+        '3 related ideas'                                 => '26',
+        'the deliberate practice guide - outline'         => '28',
+        'research deliberate practice existing resources' => '29',
+        '2 core ideas'                                    => '31',
+        'success metrics' => '32',
+
+        'deliberate practice resources for software engineers' => [
+        ],
+
+        'accelerate software development cycle' => [
         ],
     ],
 
-    'chromium white flash fix' => [
-        'My history with this bug' => '2',
-
-        'Forking and fixing a bug in 2016' => '3',
-        'thinking about vision and end goal' => '4',
-        'Someone built the windows binaries based on my work ;-)' => '5',
-        'Sleazy marketing' => '6',
-        'Conclusion' => '7',
+    'cliweb'                   => [
+        'brainstorming' => [
+            'vision'                    => '20',
+            'investigate similar ideas' => '21',
+            'redefining vision'         => '22',
+            'brainstorming prototype'   => '23',
+            'deciding on key metrics'   => '24',
+            'future of OSS'             => '25',
+        ]
     ],
 
-    'projects log' => [
-        'what is it?' => '1',
-        'design decisions' => '11',
+
+    'mouseless'                => [
+        'me and the keyboard. a love story.' => '8',
+        'increasing contributors'            => [
+            'bringing mouseless to the average user (brainstorming)' => '9',
+            'Stick to advanced users.'                               => '10',
+        ],
+        'success metrics'            => '30',
+    ],
+
+
+    'dotfiles'                 => [
+        'previous projects'  => '12',
+        'vcsh setup'         => '13',
+        'future of dotfiles' => [
+            'brainstorming ideas' => '14',
+            'analysis'            => '16',
+            'rough prototype'     => '17',
+        ],
+    ],
+
+
+    'projects log'             => [
+        'what is it?'                             => '1',
+        'tech design decisions'                   => '11',
+        'writing and self censorship'             => '15',
+        'Always have a small relevant task ready' => '27',
+    ],
+
+    'chromium white flash fix' => [
+        'My history with this bug'                                => '2',
+        'Forking and fixing a bug in 2016'                        => '3',
+        'thinking about vision and end goal'                      => '4',
+        'Someone built the windows binaries based on my work ;-)' => '5',
+        'Sleazy marketing'                                        => '6',
+        'Conclusion'                                              => '7',
+    ],
+
+
+    'business'                 => [
+        'not all business is worth it'             => '18',
+        'when eat your own dog food stops working' => '19',
     ],
 
 ];
