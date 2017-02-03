453c8a3fe25c6fd04208d86aad8884031198eeb3
hbt <hassenbentanfous@gmail.com>
2017-02-03 01:23:49 -0500

    wip

--- original/software/todos.php
+++ original/software/todos.php
@@ -2,10 +2,30 @@
 
 $todos = [
     'top' => [
-
-// TODO(hbt) NEXT do quick review of results of old searches here
         'resources for deliberate practice',
         'figure out vision, end result and metrics',
+    ],
+
+    'reorg' => [
+        'softwarestash'                                                                         => [
+            'development cycle and existing tools -- like startupstash - braintstorm ways of eliminating/enhancing the step - clear up the goal behind the step. why do it ',
+            'finding similar software ' => [
+                'biggest problem is finding the right keywords from the domain',
+                'alternative, stackshare, google',
+            ],
+        ],
+
+        'deliberate practice resources for software engineers'                                  => [
+            'list of strategies to deliberate practice',
+            'focus on exact steps for deliberate practice and clarify what each resource attempts to do',
+            'order by effectiveness based on your goals',
+        ],
+
+        'write on software'                                                                     => [
+            'remove the word feature from your vocabulary. it is an experiment first, if it doesnt work out. remove it. if it does, then you consider how it would be permanently implemented',
+            'organize it like a system instead of goals',
+        ],
+
     ],
     'get writing outline',
 
--- todos.php
+++ todos.php
@@ -1,13 +1,9 @@
 <?php
 
 $m = [
-    'top'      => [
-        'define top task for each project',
-        'define success metrics',
-        'link todos.php',
-        'brainstorm how to enhance metrics',
+    'top' => [
+        'fix projects-log on hbtlabs  + add hook and disable gh version',
     ],
-
 
     'write on' => [
         'nanopay',
@@ -15,9 +11,47 @@ $m = [
         
         'softwarestash',
         'fitness / quantified self / health -- optimal health, strength training, nutrition',
+         'growth hacking / business analysis / parnership network',
+        
+        'write on study business models / wiki. the site where you would see the business model for each company and a proper analysis',
+        'related to noticeme. if you dont understand how a company generates value, how do you expect to create it?',
+        'study and doucment the company business model',
+        'understand the funnel, the metrics, the products and how can you create value',
+        'ala stackshare maybe companies want to publish it ',
+        'startups can use them to define their business model and reuse existing components e.g subscription model, etc. as well as list their metrics and stuff',
+        'it would be a way to visualize business models and truly understand them right away and would cover other aspects of building a startup -- customer acquisition, market analysis etc. and would integrate with existing tools',
+        'http://www.digitalbusinessmodelguru.com/',
+        'http://businessmodelhub.com/photo/netflix-business-model-canvas?context=popular',
+
+
+
+
+        'review site that allowed people to build p2p business -- collaborate and share. but had a shitty business model taking a 20% cut',
+        'think about doing something similar but plmap where you would write a proposal and roadmap but get to building it',
+
+
+        'look into growth ideas and business models + brainstorming for products in stackshare' => [
+            'look for interesting products',
+            'study products with interesting business models',
+            'brainstorm related products',
+            'brainstorm growth / business opportunities',
+
+            'continue pl11.md',
+        ],
+
+
+        'write ideas about'                                                                     => [
+            'intelligence journey',
+            'software journey',
+            'business journey - business-log or projects-log to sound less gay',
+            'laptop config',
+            'write system builder idea -- share your goal and plan on how to do it, progresss, metrics, 4dx. -- people can help in your plan',
+        ],
+
     ],
 
     'admin'    => [
+
 
         'reorg' => [
 
