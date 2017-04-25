7b5b00db51f7ab08f62d5e81eda53b782ca104cd
hbt <hassenbentanfous@gmail.com>
2017-04-24 22:41:09 -0400

    wip

--- original/functionshub/todos.php
+++ original/functionshub/todos.php
@@ -2,55 +2,58 @@
 
 $t = [
 
-    'next' => [
+    'next'  => [
 
 
-        'function creation' => [
+        'function creation'    => [
 
-            'schema' => [
-                'create basic version',
+            'use docker compose for active dev - document workflow + decisions' => [
+
             ],
 
-            'fn def' => [
-                'namespace',
-                'name',
-                'main and test',
-                'tests',
-                'version',
-
-                // Note(hbt) use socket to build and run. generated docker name is fine and auto-populate it in fn def -- double check same hashcodes are produced -- use the version
-                // rethink these 2 - if needed, users shouldnt have to set them
-                'image',
-                'containername',
+            'fix build and run and update tests'                                => [
+                'build on the fly and look up the function in fndef.yml',
             ],
 
-            'figure out how the dev-helper will communicate via docker',
 
-
-            'later'  => [
-
-                'fix dc down on server -- reduce timeout',
-                'replace copy by a dc mount volume for /function -- verify dockerignore works ',
-
-                'extract watch as a local cli command ',
-                
-                'add auto-detection / warning about proxies -- ' => [
-                    'Creating network "php_default" with the default driver
-ERROR: could not find an available, non-overlapping IPv4 address pool among the defaults to assign to the network
-'
-                ],
-
-                'fix watch to use wait #127 or add queue + move singleton to branch -- add issue manager',
-
-                'make repo private'                              => [
-                    'check backup system works with private git repos',
-                ],
-
-                'inv yml/json schemal validaton',
-            ],
         ],
 
+        'add input support'    => [
 
+        ],
+
+        'video-mp3 functions'  => [
+
+        ],
+
+        'add stress on gcloud' => [
+
+        ],
+
+    ],
+
+    'later' => [
+
+        'figure out how the dev-helper will communicate via docker',
+
+        'fix dc down on server -- reduce timeout',
+        'replace copy by a dc mount volume for /function -- verify dockerignore works ',
+
+        'extract watch as a local cli command ',
+
+        'add auto-detection / warning about proxies -- ' => [
+            'Creating network "php_default" with the default driver
+ERROR: could not find an available, non-overlapping IPv4 address pool among the defaults to assign to the network
+'
+        ],
+
+        'fix watch to use wait #127 or add queue + move singleton to branch -- add issue manager',
+
+        'make repo private'                              => [
+            'check backup system works with private git repos',
+        ],
+
+        'inv yml/json schemal validaton',
     ],
 
 
