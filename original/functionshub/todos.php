ea7ac292316f4f571cedbea7d0d977673aabbcbf
hbt <hassenbentanfous@gmail.com>
2017-04-24 21:33:16 -0400

    auto-update from boot script

--- original/functionshub/todos.php
+++ original/functionshub/todos.php
@@ -2,27 +2,11 @@
 
 $t = [
 
-    'next'  => [
-        
-//        Running and testing
-        'dc ',
-        'inv ',
-        'inv' => [
+    'next' => [
 
-            // Use Docker compose down but reduce the time instead of kill
-            'dc down sleep',
-
-            'dc up multi',
-
-        ],
 
         'function creation' => [
-            'clean gkeep',
-            'fix watch to use wait #127 or add queue + move singleton to branch -- add issue manager',
 
-            'figure out how the dev-helper will communicate via docker',
-
-            'inv yml/json schemal validaton',
             'schema' => [
                 'create basic version',
             ],
@@ -38,6 +22,31 @@ $t = [
                 // rethink these 2 - if needed, users shouldnt have to set them
                 'image',
                 'containername',
+            ],
+
+            'figure out how the dev-helper will communicate via docker',
+
+
+            'later'  => [
+
+                'fix dc down on server -- reduce timeout',
+                'replace copy by a dc mount volume for /function -- verify dockerignore works ',
+
+                'extract watch as a local cli command ',
+                
+                'add auto-detection / warning about proxies -- ' => [
+                    'Creating network "php_default" with the default driver
+ERROR: could not find an available, non-overlapping IPv4 address pool among the defaults to assign to the network
+'
+                ],
+
+                'fix watch to use wait #127 or add queue + move singleton to branch -- add issue manager',
+
+                'make repo private'                              => [
+                    'check backup system works with private git repos',
+                ],
+
+                'inv yml/json schemal validaton',
             ],
         ],
 
