10b64eaf865641be9bf2cde8bb7d9aa9fbefa05e
hbt <hassenbentanfous@gmail.com>
2017-05-04 11:54:37 -0400

    auto-update from boot script

--- original/functionshub/todos.php
+++ original/functionshub/todos.php
@@ -7,97 +7,50 @@ $t = [
 
         'function creation'           => [
 
-            'endgoal' => [
-
-//                'doc workflow'           => [
-//                ],
-
-//                'fix crash msg'          => [
-//
-//                ],
-
-                'fix current workflow'   => [
-
-//                    'fix ar' => [
-//                        
-//                    ],
-
-//                    'add skeleton'               => [
-//                        'files',
-//                        'readme',
-//                    ],
-                    
-
-//                    'add build all'              => [
-//                        'get fndef',
-//                        'build docker',
-//                        'store version',
-//                    ],
-
-//                    'fix version' => [
-//                        'add version',
-//                        'add ignore',
-//                        'fix build all',
-//                        'update skeleton',
-//                    ],
-
-//                    'fix run'                    => [
-////                        'mv run to fns',
-////                        'flag js',
-////                        'add simple run test ',
-//                        'reuse debug flow',
-//                        'run',
-//                    ],
-
-
-//                    'add remote runner with api' => [
-//                        'test',
-//                        'deploy and pass',
-//                    ],
-
-
-//                    'test with hello php'        => [
-//                    ],
-                    
-                ],
-
-
-                'move hello/js and test' => [
-                ],
-
-                'srm hello2'             => [
-                ],
-
-
-            ],
-
-
         ],
 
 
-        'fix image id' => [
-        ],
 
         'brainstorm mp3 example flow' => [
+            'functions' => [
+                'youtubedl function' => [
+//                    'fn',
+//                    'docker test',
+                    'raw test',
+                    'add storage service',
+                ],
+                'mp3 converter' => [
+                ],
+                'gdrive upload' => [
+                    // inv hashicopr vault and token storage
+                ],
+                'del file' => [
+                ],
+                'multiple links' => [
+                ],
+            ],
         ],
 
-        'add input support'           => [
-        ],
 
-        'video-mp3 functions'         => [
-        ],
 
-        'refactor' => [
-        ],
-
-        'add stress on gcloud'        => [
-
-        ],
 
     ],
 
     'later' => [
 
+        'add type checking for input' => [
+        ],
+
+        'improve dev process and make it more resilient and better error messages when creating functions' => [
+            'also remove reliance on cl(body) for debugging. ',
+        ],
+
+        'add stress on gcloud'        => [
+        ],
+
+        'fix image id' => [
+            '/run different from /debug',
+        ],
         'figure out how the dev-helper will communicate via docker',
 
         'fix dc down on server -- reduce timeout',
@@ -111,10 +64,18 @@ ERROR: could not find an available, non-overlapping IPv4 address pool among the 
 '
         ],
 
-        'fix watch to use wait #127 or add queue + move singleton to branch -- add issue manager',
+        'watch' => [
+            'fix watch to use wait #127 or add queue + move singleton to branch -- add issue manager',
+            'https://github.com/mikeal/watch/pull/127#issuecomment-296496818',
+        ],
 
         'make repo private'                              => [
             'check backup system works with private git repos',
+        ],
+
+        'set up gitlab backup' => [
+            'dump repo',
+            'dump issues',
         ],
 
         'inv yml/json schemal validaton',
