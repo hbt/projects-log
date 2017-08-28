44868a2c452014631f1beb65cf1ae2c714600421
hbt <hassenbentanfous@gmail.com>
2017-08-28 17:32:26 -0400

    wip

--- original/mouseless/todos.php
+++ original/mouseless/todos.php
@@ -1,69 +1,79 @@
 <?php
 
 $todos = [
-    'top'              => [
 
-        'next' => [
+    'todos'     => [
+        'top' => [
 
-
-
-            'pick next tickets',
+            'next' => [
 
 
-            'next week' => [
+                'pick next tickets',
 
-            ],
-        ],
 
-    ],
-
-    'weekly checklist' => [
-        'review forks -- last count 134',
-        'review commits + merge ',
-        'check original fork' => [
-            'git co 1995',
-            'git pull git://github.com/1995eaton/chromium-vim.git master',
-            'git log dev..chromium-vim',
-            'review and merge',
-        ],
-        'merge pull requests' => [
-            'checkout ',
-            'examples' => [
-                'fetch e.g git pull git://github.com/rosshadden/mouseless.git fix/editing-divs',
-                'tarao' => [
-                    'g co -b tarao',
-                    'git pull git://github.com/tarao/chromium-vim.git tarao:tarao',
+                'next week' => [
+                    // TODO(hbt) NEXT fix recently added bookmark commands  -- view commit checklist
+                    'https://github.com/hbt/mouseless/issues/126',
                 ],
             ],
-            'merge in dev',
-            'notify author',
-            'update changelog',
-            'add to docs/thanks',
-            'update docs/commands',
-            './scripts/generate-docs',
+
         ],
     ],
 
-    'commit checklist' => [
-        'issue is elimnated and cannot be reopened',
-        'commit on dev',
-        'commit msg contains ticket number',
-        'update commands.adoc/settings.adoc',
-        'update changelog',
-        './scripts/generate-docs',
-        'review diff',
-        'push',
-        'close ticket',
-    ],
+    'checklist' => [
+        'weekly checklist'     => [
 
-    'releease'         => [
-        '. edit manifest.json and update version
-. update changelog.adoc
-. render all adoc files
-. ./scripts/release.sh 1.3.0
-'
-//        'https://hbt.github.io/mouseless/tips.html#_how_to_do_a_release',
-    ],
+            'check new changes from main repo worth having' => [
+                'review forks in network -- last count 141',
+                'review commits + merge ',
 
+                'merging checklist' => [
+                    'check original fork' => [
+                        'git co 1995',
+                        'git pull git://github.com/1995eaton/chromium-vim.git master',
+                        'git log dev..chromium-vim',
+                        'review and merge',
+                    ],
+                    'merge pull requests' => [
+                        'checkout ',
+                        'examples' => [
+                            'fetch e.g git pull git://github.com/rosshadden/mouseless.git fix/editing-divs',
+                            'tarao' => [
+                                'g co -b tarao',
+                                'git pull git://github.com/tarao/chromium-vim.git tarao:tarao',
+                            ],
+                        ],
+                        'merge in dev',
+                        'notify author',
+                        'update changelog',
+                        'add to docs/thanks',
+                        'update docs/commands',
+                        './scripts/generate-docs',
+                    ],
+                ],
+            ],
+        ],
+
+        'new commit checklist' => [
+            'issue is elimnated and cannot be reopened',
+            'commit on dev',
+            'commit msg contains ticket number',
+            'update commands.adoc/settings.adoc',
+            'update changelog',
+            './scripts/generate-docs',
+            'review diff',
+            'push',
+            'close ticket',
+        ],
+
+        'releease checklist'   => [
+            '. edit manifest.json and update version',
+            '. update changelog.adoc',
+            ' render all adoc files',
+            ' ./scripts/release.sh 1.3.0',
+//        https://hbt.github.io/mouseless/tips.html#_how_to_do_a_release',
+        ],
+
+    ],
 ];
 ?>
